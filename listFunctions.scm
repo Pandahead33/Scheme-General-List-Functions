868aa59ac65d7789a6b1758e40cb980e3355ef3e
Contextual <igetspammed1234@gmail.com>
2018-04-03 22:49:05 -0400

    Created all list functions
    Could be expanded but this is all I plan on doing for now

---
+++ listFunctions.scm
@@ -0,0 +1,40 @@
+(define (first_element f)
+  display (car f))
+  
+
+(define (last_element l)
+  (cond ((null? (cdr l)) display (car l))
+        (else (last_element (cdr l)))
+                ))
+          
+(define (next_cons a b) 
+  (cond 
+        ((null? a) ())
+        ((not (null? (cdr a))) (cons (car a) (next_cons (cdr a) b)))
+        (else (cons (car a) (next_cons b ())))
+  )
+)
+
+(define (append_lists a b)
+  (cond 
+        ((null? (cdr a)) (cons (car a) (next_cons b ())))
+        (else (cons (car a) (next_cons (cdr a) b)))
+  )
+)
+
+(define (remove_last_element a)
+ (cond 
+    ((null? (cdr a)) ())
+    (else (cons(car a) (remove_last_element (cdr a))))   
+ )
+)
+
+
+(define (reverse_list a)
+  (cond 
+    ((null? (cdr a)) (cons (car a) ()))
+    (else 
+          (cons (last_element a) (reverse_list(remove_last_element a)))
+    )
+  )
+)
